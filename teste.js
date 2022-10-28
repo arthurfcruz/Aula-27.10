b111a8315c866d5abd0dc4c89953ba5f3e516f37
Arthur <arthurfelixcruz@gmail.com>
2022-10-27 21:20:07 -0300

    criando o cadastro dos produtos

---
+++ teste.js
@@ -0,0 +1,12 @@
+var nomes = []
+var ids = []
+var precos = []
+var avaliacao = []
+
+function cadastrarProduto(){ 
+    nomes = prompt("digite o nome do produto para cadastrar")
+    ids = prompt("digite a ID do produto para cadastrar")
+    precos = prompt("digite o preço do produto para cadastrar")
+    avaliacao = prompt("digite a avaliacao do produto para cadastrar")
+} 
+
